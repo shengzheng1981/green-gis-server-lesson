1ba4e95e9693e9e90317199df0494dcaa11a7170
shengzheng1981 <11639583@qq.com>
2020-11-09 13:09:08 +0800

    lesson03

--- lesson03/routes/feature-classes.js
+++ lesson03/routes/feature-classes.js
@@ -30,106 +30,79 @@ router.get('/all', (req, res, next) => {
 });
 
 /* 发布shapefile. */
-router.post('/publish/shapefile/:name', extendTimeout, shape_upload.array('file'),  (req, res, next) => {
-    // move the file from the temporary location to the intended location
+router.post('/publish/shapefile/:name', extendTimeout, shape_upload.array('file'),  async (req, res, next) => {
     // 上传shapefile
-    let shapefilename = "";
-    req.files.forEach( file => {
-        const origin_path = file.path;
-        const target_path = path.join(shape_path, file.originalname);
-        fs.renameSync(origin_path, target_path);
-        const array = file.originalname.split('.');
-        const file_ext = array[array.length - 1];
-        if (file_ext.toLowerCase() === "shp") shapefilename = file.originalname;
-    });
-    if (!shapefilename) {
-        res.status(200);
-        res.json({
-            result: false,
-            message: "shapefile not found!"
+    try {
+        let shapefilename = "";
+        req.files.forEach( file => {
+            const origin_path = file.path;
+            const target_path = path.join(shape_path, file.originalname);
+            // move the file from the temporary location to the intended location
+            fs.renameSync(origin_path, target_path);
+            const array = file.originalname.split('.');
+            const file_ext = array[array.length - 1];
+            if (file_ext.toLowerCase() === "shp") shapefilename = file.originalname;
         });
-        return;
-    }
-
-    // 打开shapefile
-    const name = req.params.name;
-    const shapefile = path.join(path.join(path.join(path.dirname(__dirname), 'public'),'shapes'), shapefilename);
-    const ds = gdal.open(shapefile);
-    const layer = ds.layers.get(0);
-
-    // 创建feature class
-    FeatureClass.findOne({
-        name: name
-    }).exec( (err, cls) => {
-        if (err) {
-            res.status(500);
-            res.json(err);
-        } else{
-            if (cls) {
-                res.status(200);
-                res.json({
-                    result: false,
-                    message: "name exists!"
-                });
-            }else{
-                cls = {
-                    name: name,
-                    geotype: layer.geomType,
-                    tile: true,
-                    fields: []
-                };
-                layer.fields.forEach( item => {
-                    cls.fields.push({
-                        name: item.name,
-                        type: item.type
-                    })
-                });
-                FeatureClass.create(cls, (err, doc) => {
-                    if (err) {
-                        res.status(500);
-                        res.json(err);
-                    }
-                    else{
-                        // 添加 schema
-                        schema.add(doc.toObject());
-                        const features = [];
-                        const model = schema.model(name);
-                        const srs = gdal.SpatialReference.fromEPSG(4326);
-                        layer.features.forEach( item => {
-                            try{
-                                const geometry = item.getGeometry();
-                                // 转换坐标
-                                geometry.transformTo(srs);
-                                // 计算空间索引
-                                const feature = tile.calc({
-                                    geometry: JSON.parse(geometry.toJSON()),
-                                    properties: JSON.parse(item.fields.toJSON())
-                                });
-                                features.push(model(feature));
-                            } catch (e) {
-                                console.log(e);
-                            }
-                        });
-                        ds.close();
-                        model.insertMany(features, (err, docs)=>{
-                            if (err) {
-                                res.status(500);
-                                res.json(err);
-                            } else {
-                                console.log( name + ' published!');
-                                res.status(200);
-                                res.json({
-                                    result: true,
-                                    doc : doc
-                                });
-                            }
-                        });
-
-                    }
-                });
-            }
+        // 上传文件无shp后缀
+        if (!shapefilename) {
+            throw new Error("shapefile not found!");
         }
-    });
+        // 创建feature class
+        let cls = await FeatureClass.findOne({name: req.params.name});
+        if (cls) {
+            throw new Error("class name exists!");
+        } else {
+            // 打开shapefile
+            const shapefile = path.join(path.join(path.join(path.dirname(__dirname), 'public'),'shapes'), shapefilename);
+            const ds = gdal.open(shapefile);
+            const layer = ds.layers.get(0);
+            cls = {
+                name: req.params.name,
+                geotype: layer.geomType,
+                tile: true,
+                fields: []
+            };
+            layer.fields.forEach( item => {
+                cls.fields.push({
+                    name: item.name,
+                    type: item.type
+                })
+            });
+            const doc = await FeatureClass.create(cls);
+            // 添加 schema
+            schema.add(doc.toObject());
+            const features = [];
+            const model = schema.model(req.params.name);
+            const srs = gdal.SpatialReference.fromEPSG(4326);
+            layer.features.forEach( item => {
+                try{
+                    const geometry = item.getGeometry();
+                    // 转换坐标
+                    geometry.transformTo(srs);
+                    // 计算空间索引
+                    const feature = tile.calc({
+                        geometry: JSON.parse(geometry.toJSON()),
+                        properties: JSON.parse(item.fields.toJSON())
+                    });
+                    features.push(model(feature));
+                } catch (e) {
+                    console.log(e);
+                }
+            });
+            ds.close();
+            // 批量插入矢量要素
+            await model.insertMany(features);
+            console.log( req.params.name + ' ' + features.length +' published!');
+            res.status(200);
+            res.json({
+                result: true,
+                doc : doc
+            });
+        }
+    } catch (err) {
+        res.status(500);
+        res.json(err.message);
+    }
 });
 
 /* 根据名称删除class. */
